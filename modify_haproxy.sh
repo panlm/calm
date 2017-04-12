a4f17883972d08d4ed72387436ad0fc951b0272b
panlm <stevenpan@gmail.com>
2017-04-13 06:48:14 +0800

    minor

--- modify_haproxy.sh
+++ modify_haproxy.sh
@@ -5,7 +5,7 @@ cp haproxy.cfg haproxy.cfg.`date +%Y%m%d%H%M%S`
 sed -i '/^listen wordpress 0.0.0.0:80$/,/^$/d' haproxy.cfg
 cat ~/git/haproxy_wordpress.cfg >>haproxy.cfg
 
-cat /etc/hosts |grep -E 'wordpress[0-9]\+' |awk '{print $2,$1}' |sed -e 's/^/    server /' -e 's/$/:80 check/' >>haproxy.cfg
+cat /etc/hosts |grep -E 'wordpress[0-9]+' |awk '{print $2,$1}' |sed -e 's/^/    server /' -e 's/$/:80 check/' >>haproxy.cfg
 
 systemctl restart haproxy
 
