576ef6548783613591b3c78ba75f16831d7dcca4
panlm <stevenpan@gmail.com>
2017-04-13 06:07:02 +0800

    update createwordpressdb.sql

--- createwordpressdb.sql
+++ createwordpressdb.sql
@@ -1,8 +1,7 @@
-
-
-
 CREATE DATABASE wordpress;
-CREATE USER wordpressuser@localhost IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost IDENTIFIED BY 'password';
+CREATE USER 'wordpressuser'@'%' IDENTIFIED BY 'password';
+CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
 
