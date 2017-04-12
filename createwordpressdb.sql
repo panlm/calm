7047ac1acea22f56648f4c3fe30899f3e4131746
panlm <stevenpan@gmail.com>
2017-04-13 05:00:40 +0800

    create wordpress db

---
+++ createwordpressdb.sql
@@ -0,0 +1,8 @@
+
+
+
+CREATE DATABASE wordpress;
+CREATE USER wordpressuser@localhost IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+
