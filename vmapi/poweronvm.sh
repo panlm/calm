5e7194faffbdc8b10b6d12c1330bd028479374fd
stevenpan <stevenpan@stevenpans-MacBook-Pro.local>
2017-05-17 15:14:34 +0800

    update vm on/off/reset

--- vmapi/poweronvm.sh
+++ vmapi/poweronvm.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-if [ $# -gt 2 ]; then
-    echo "Usage: ./$0 [vmuuid]"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 [vmuuid] [on|off|reset]"
     exit 9
 fi
 
-case $# in
-0)
-    ./listvm.sh
-;;
-1)
-    uuid=$1
-    apifile=api-poweronvm
-    runfile=run-poweronvm
-    cat $apifile |sed -e 's/uuidstring/'"$uuid"'/' >$runfile 2>&1
-    chmod a+x $runfile
-    ./$runfile >${0%.*}.log 2>&1
-;;
-*)
-    exit
-;;
-esac
+operationstring=`echo $2 |tr [a-z] [A-Z]`
+
+if [ ! $operationstring == 'ON' ]; then
+    if [ ! $operationstring == 'OFF' ]; then
+        if [ ! $operationstring == 'RESET' ]; then
+            echo "Usage: $0 [vmuuid] [on|off|reset]"
+            exit 9
+        fi
+    fi
+fi
+
+uuid=$1
+apifile=api-poweronvm
+runfile=run-poweronvm
+cat $apifile |sed -e 's/uuidstring/'"$uuid"'/' \
+    -e '/transition/s/ON/'"$operationstring"'/' >$runfile 2>&1
+chmod a+x $runfile
+
+./$runfile >${0%.*}.log 2>&1
 
