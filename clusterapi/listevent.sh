08e4fd5ad3b752cb3e5028de2bcd501d652d8932
panlm <stevenpan@gmail.com>
2017-07-22 22:23:32 +0800

    support multicluster collection

--- clusterapi/listevent.sh
+++ clusterapi/listevent.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 start_time end_time"
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 cluster1_ip cluster2_ip ... "
     exit 9
 fi
 
-#if [ -x /usr/bin/in2csv ]; then
-#    STR=" |jq -s . |in2csv --format json - |csvlook"
-#else
-#    STR=""
-#fi
+export PATH=$PATH:/usr/local/bin
+umask 0000
+timestamp=`date +%s`
 
-string1="start_time_in_usecs="$1
-string2="end_time_in_usecs="$2
+for i in $* ; do
+    cluster=$i
+    path=`dirname $0`
+    timefile=/var/tmp/$cluster-listevent.sh.timestamp
+    outfile=/var/tmp/$cluster-listevent.sh.out.$timestamp
+    apifile=$path/api-listevent
+    runfile=$path/run-listevent-$cluster
 
-apifile=api-listevent
-runfile=run-listevent
+    end_time=${timestamp}000000
+    if [ -f $timefile ]; then
+        start_time=`cat $timefile`
+    else
+        start_time=`echo $timestamp - 3600 |bc `000000
+    fi
+    
+    #if [ -x /usr/bin/in2csv ]; then
+    #    STR=" |jq -s . |in2csv --format json - |csvlook"
+    #else
+    #    STR=""
+    #fi
+    
+    string1="start_time_in_usecs="$start_time
+    string2="end_time_in_usecs="$end_time
+    
+    cat $apifile >$runfile
+    sed -i "/https/s/events\/'/events\/?$string1\&$string2'/" $runfile
+    sed -i "/https/s/10\.132\.68\.45/$cluster/" $runfile
+    chmod a+x $runfile
+    
+    #save all alert to tmpfile
+    ./$runfile 2>/dev/null |jq -r '.' >$outfile
+    echo $end_time > $timefile
+done
 
-cat $apifile >$runfile
-sed -i "/https/s/events\/'/events\/?$string1\&$string2'/" $runfile
-chmod a+x $runfile
-
-#save all alert to tmpfile
-./$runfile 2>/dev/null |jq -r '.entities[]' >out-listevent
-
-
+exit 0
 
 
 
@@ -33,8 +52,6 @@ chmod a+x $runfile
 
 #./$runfile 2>/dev/null |jq -r '.entities[] | select ( .alert_type_uuid == \"LoginInfoAudit\" ) | {alert_type_uuid, created_time_stamp_in_usecs, severity}'
 
-
-exit 0
 
 
 #cmd="./$runfile 2>/dev/null |jq -r '.entities[] | {id, alert_type_uuid, message}' "$STR
